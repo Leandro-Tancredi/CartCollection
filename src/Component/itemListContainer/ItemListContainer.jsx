ff8398a3ebc5e4eba6a97ddf02c17619942b0481
Facundo Quiroga <facundo.quiroga@mjus.gba.gov.ar>
2022-08-25 11:26:04 -0300

    identación

--- src/Component/itemListContainer/ItemListContainer.jsx
+++ src/Component/itemListContainer/ItemListContainer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Boton from "./itemCount";
 const ItemListContainer = ({ greeting }) => {
-    const onAdd=(valor)=>{
-console.log(`usted compró ${valor} unidades`);
+    const onAdd = (valor) => {
+        console.log(`usted compró ${valor} unidades`);
     }
     return (<div>
         <h1 style={{ color: "blue" }}>
-            {greeting}         
+            {greeting}
         </h1>
-    <Boton Stock={5} initial={0} onAdd={onAdd} />
+        <Boton Stock={5} initial={0} onAdd={onAdd} />
     </div>
     )
 }
